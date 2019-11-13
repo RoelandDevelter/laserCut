2943f672952dbe0c30306d5ab8dcf7e4702741aa
Roeland Develter <56031454+RoelandDevelter@users.noreply.github.com>
2019-11-13 09:53:21 +0100

    change parameters

--- Sunflower.py
+++ Sunflower.py
@@ -1,16 +1,16 @@
 import svgwrite
 import numpy as np
 
-drawing = svgwrite.Drawing(filename = "sunflower.svg", size = (500, 500))
+drawing = svgwrite.Drawing(filename = "sunflower.svg", size = (1000, 1000))
 
 seeds = 2000
-cst = 3.4
+cst = 7
 angle = np.pi * 137.5 / 180
-center = 250
+center = 500
 
 for n in range(1,seeds+1):
     x = center + cst * np.sqrt(n) * np.cos(n * angle)
     y = center + cst * np.sqrt(n) * np.sin(n * angle)
-    drawing.add(drawing.circle(center = (x,y), r =2, fill = 'black'))
+    drawing.add(drawing.circle(center = (x,y), r =5, fill = 'black'))
 
 drawing.save()
