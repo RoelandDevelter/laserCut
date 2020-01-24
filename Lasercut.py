cf5d7c18968a21ae30afccea3fa0a6bec79f2629
Roeland Develter <56031454+RoelandDevelter@users.noreply.github.com>
2020-01-24 12:21:50 +0100

    modified

--- Lasercut.py
+++ Lasercut.py
@@ -1,50 +1,84 @@
 import numpy as np
+import time
 
-# things tried: np.vectorize(lambda number: format(number, 'b').zfill(n), numbers)
-# [bin(number) for number in numbers] -> not zero padded
-# [format(number, 'b').zfill(n) for number in numbers] -> zero padded
-# [np.binary_repr(number) for number in numbers]
+"""
+things tried:   np.vectorize(lambda number: format(number, 'b').zfill(n), numbers)
+                [bin(number) for number in numbers] -> not zero padded
+                [format(number, 'b').zfill(n) for number in numbers] -> zero padded
+                [np.binary_repr(number) for number in numbers]
+"""
 
 
+def timer(func):
+    """A decorator that prints how long a function took to run."""
+    def wrapper(*args, **kwargs):
+        t_start = time.time()
+        result = func(*args, **kwargs)
+        t_total = time.time() - t_start
+        print('{} took {}s'.format(func.__name__, t_total))
+        return result
+    return wrapper
+
+@timer
 def generate_code(no_bits, type = 'natural'):
-    # generates codes. Lucal code does not add a different pattern
+    """ Generates binary codes (i.e. counts through different bit representations). Lucal code does not add a different pattern
+
+    Parameters
+    ----------
+    no_bits : int
+        Number of bits the code should be.
+    type : str, optional
+        The type of code generated. Possible parameters: natural, gray
+        Default=natural.
+
+    Returns
+    -------
+    The type of the return value
+        Can include a description of the return value.
+        Replace "Returns" with "Yields" if this function is a generator.
+    """
     if type == 'natural':
         code = [list(format(number, 'b').zfill(no_bits)) for number in range(2**no_bits)]
     elif type == 'gray':
         code = [list(format(number ^ (number >> 1), 'b').zfill(no_bits)) for number in range(2**no_bits)]
-    return np.array(code) == '1'   # convert to boolean array
+    return np.array(code, dtype = bool)
 
 
 def detect_edges(code):
-    # axis 0 == 'vertical', axis 1 == 'horizontal'
-    # returns edge_x, edge_y
-    x, y = np.shape(code)
-    edge_x = np.logical_xor(code, np.roll(code, 1, axis = 1))
-    edge_x = (np.ones((x,y)) * np.arange(y))[edge_x]
-    print(edge_x)
-    edge_y = np.logical_xor(code, np.roll(code, 1, axis = 0))
-    edge_y = (np.ones((x,y)) * np.arange(y))[edge_y]
-    # return coordinates
-    return edge_x, edge_y
+    """ Detects edges on given code. 
 
+    Parameters
+    ----------
+    code: 2D-numpy array
+        Code to detect edges on
 
-def find_boundaries(seq, color_val = 1):
-    vals = []
-    no_values = len(seq)
-    no_bits = len(seq[0])
-    for i in range(no_bits):
-        begin = 0
-        end = 0
-        for j in range(no_values):
-            if seq[j,i] == color_val:
-                # if color switch has happened -> beginning
-                if seq[(j-1)%(no_values), i] != color_val:
-                    begin = j               
-                # if color switch will happen -> ending
-                if (seq[(j+1)%(no_values), i] != color_val) or (seq[(j+1)%(no_values), i] == 0):
-                    end = j+1
-                    vals.append((i, begin, end))
-    return vals
+    Returns
+    -------
+    edge_r, edge_theta: 2D-numpy arrays, edge_theta same size as code, edge_r 1 column more
+        The r- and theta-direction edges.
+    """
+    no_values, no_bits = np.shape(code)
+    # axis 0 == 'theta-direction', axis 1 == 'r-direction'
+    edge_theta = np.logical_xor(code, np.roll(code, 1, axis = 0))
+
+    # for r-direction, add zero row in beginning ("inner empty circle") and at the end ("outer empty circle") and xor this
+    # this gives an extra column to the edge data 
+    code1 = np.zeros((no_values, no_bits+1))
+    code1[:,:-1] = code
+    code2 = np.zeros((no_values, no_bits+1))
+    code2[:,1:] = code
+
+    edge_r = np.logical_xor(code1, code2)
+    
+    # create coordinate matrix
+    mat = np.ones((no_values, no_bits+1,2))
+    mat[:,:,1] = np.arange(no_values).reshape(no_values,1)
+    mat[:,:,0] = np.arange(no_bits+1)
+
+    edge_r = mat[edge_r] 
+    edge_theta = mat[:,:no_bits][edge_theta]
+
+    return edge_r, edge_theta
 
 
 def make_path(edge_x, edge_y):
@@ -64,16 +98,9 @@ def write_img(svg_path):
 
 
 def main():
-    # print(timeit.timeit("generate_gray_np(8)", setup="from __main__ import generate_gray_np", number = 20))
-    # print(timeit.timeit("generate_gray(8)", setup = "from __main__ import generate_gray", number = 20))
-    code = generate_code(2, type = 'natural')
-    code_manual = np.array([[False, False, False, False, False], [True, True,True,True,True], [False, False, False, False, False]])
-    edge_x, edge_y = detect_edges(code_manual)
-    vals = find_boundaries(code_manual)
-    print(code_manual)
-    print(vals)
-    print(edge_x)
-    print(edge_y)
+    code = generate_code(20, type = 'natural')
+    # edge_r, edge_theta = detect_edges(code)
+    print('breakpoint')
 
 
 if __name__ == "__main__":
